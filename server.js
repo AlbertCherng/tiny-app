b2964c2709ce01397f01fdae2026ca4002eb3c40
vagrant <albert.cherng@gmail.com>
2016-07-03 23:18:06 +0000

    Refactor with Raf

--- server.js
+++ server.js
@@ -2,29 +2,38 @@
 
 const methodOverride = require('method-override');
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 8080;
-app.set("view engine", "ejs");
+const Mongo = require("mongodb");
+const bodyParser = require("body-parser");
 
-const Mongo       = require("mongodb");
+const PORT = process.env.PORT || 8080;
+
 const MongoClient = Mongo.MongoClient;
 const MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true}));
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let randomString = "";
+  const charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+  for(let i = 0; i < 6; i++) {
+    randomString += charset.charAt(Math.floor(Math.random() * charset.length));
+  };
+  return randomString;
+};
 
 let dbInstance;
-
 MongoClient.connect(MONGODB_URI, (err, db) => {
-  if (err) {
-    throw err;
-  };
+  if (err) { throw err; };
   console.log(`Successfully connected to DB: ${MONGODB_URI}`);
   dbInstance = db;
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Server started on port ${PORT}!`);
 });
 
 app.get("/", (req, res) => {
@@ -33,58 +42,52 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   dbInstance.collection("urls").find().toArray((err, results) => {
-    let templateVars = {urls: results};
-    res.render("urls_index", templateVars);
+    res.render("urls_index", {urls: results});
   });
-});
-
-app.post("/urls", (req, res) => {
-  function generateRandomString() {
-    let randomString = "";
-    let charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-      for( let i=0; i < 6; i++ ) {
-        randomString += charset.charAt(Math.floor(Math.random() * charset.length));
-      };
-    return randomString;
-  };
-  let inputURL = req.body.URL;
-  let rCode = generateRandomString();
-  dbInstance.collection("urls").insertOne({shortURL: rCode, longURL: inputURL});
-  res.redirect("/urls");
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  dbInstance.collection("urls").insertOne({shortURL: generateRandomString(),
+                                           longURL: req.body.URL});
+  // TODO: should be in a callback:
+  res.redirect("/urls");
+});
+
 app.get("/urls/:id", (req, res) => {
-  dbInstance.collection("urls").findOne({shortURL:req.params.id }, (err, result) => {
+  dbInstance.collection("urls").findOne({shortURL: req.params.id }, (err, result) => {
     res.render("urls_show", result);
   });
 });
 
-app.delete("/urls/:id", (req, res) => {
-  dbInstance.collection("urls").deleteOne({shortURL:req.params.id}, (err, result) => {
-  res.redirect("/urls");
-  });
-});
-
 app.get("/urls/:id/edit", (req, res) => {
-  dbInstance.collection("urls").findOne({shortURL:req.params.id}, (err, result) => {
+  dbInstance.collection("urls").findOne({shortURL: req.params.id}, (err, result) => {
     res.render("urls_edit", result);
   });
 });
 
-app.put("/urls/:id/edit", (req, res) => {
-  dbInstance.collection("urls").updateOne({shortURL:req.params.id},
+app.put("/urls/:id", (req, res) => {
+  dbInstance.collection("urls").updateOne({shortURL: req.params.id},
                                           {$set: {longURL: req.body.newURL}},
                                           (err, result) => {
-    res.redirect("/urls");
-  });
+                                            res.redirect("/urls");
+                                          });
+});
+
+app.delete("/urls/:id", (req, res) => {
+  dbInstance
+    .collection("urls")
+    .deleteOne({shortURL: req.params.id},
+               (err, result) => {
+                res.redirect("/urls");
+               });
 });
 
 app.get("/u/:id", (req, res) => {
-  dbInstance.collection("urls").findOne({shortURL:req.params.id}, (err, result) => {
-  res.redirect(result.longURL);
+  dbInstance.collection("urls").findOne({shortURL: req.params.id}, (err, result) => {
+    res.redirect(result.longURL);
   });
 });
